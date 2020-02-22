c591c0dc986631faaedaefa5080b9f9dbabbd6b3
Johnny Magrippis <j@magrippis.com>
2020-02-22 11:57:17 +0000

    scaffold API with endpoint that connects to postgres

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import fetch from 'node-fetch'
-import { sample } from 'lodash'
-import { parseEmojis } from './parseEmojis'
+import express from 'express'
 
-const EMOJI_LIST_URL = 'https://unicode.org/emoji/charts/full-emoji-list.html'
+import { pool } from './pool'
 
-fetch(EMOJI_LIST_URL).then(res =>
-  res.text().then(html => {
-    const emojis = parseEmojis(html)
+const app = express().get('/day', async (_, res) => {
+  const client = await pool.connect()
 
-    console.log(emojis.length)
-    console.log(sample(emojis))
-  })
-)
+  try {
+    const {
+      rows: [emoji],
+    } = await client.query(
+      'SELECT character, name FROM emojis WHERE type = "day" LIMIT 1'
+    )
+
+    res.end(JSON.stringify(emoji))
+  } catch (err) {
+    console.error(err)
+
+    res.status(500)
+    res.end('Error ' + err)
+  } finally {
+    client.release()
+  }
+})
+
+app.listen(process.env.PORT || 3400)
---
+++ src/tasks/pick.ts
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+import { sample } from 'lodash'
+
+import { parseEmojis } from '../parseEmojis'
+
+const EMOJI_LIST_URL = 'https://unicode.org/emoji/charts/full-emoji-list.html'
+
+fetch(EMOJI_LIST_URL).then(res =>
+  res.text().then(html => {
+    const emojis = parseEmojis(html)
+
+    console.log(emojis.length)
+    console.log(sample(emojis))
+  })
+)
