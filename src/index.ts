8b996b3ad13a010979ae32e3355fc2647f10b018
Johnny Magrippis <j@magrippis.com>
2020-02-23 16:51:06 +0000

    use cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 
 import { dayHandler } from './routes/day'
 import { triosRouter } from './routes/trios/router'
 
 const app = express()
+  .use(cors())
   .get('/day', dayHandler)
   .use('/trios', triosRouter)
 
