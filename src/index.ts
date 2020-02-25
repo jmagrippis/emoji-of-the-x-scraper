e6c823b6c9f0db02f7d4dc76acf15300d2a4a0b4
Johnny Magrippis <j@magrippis.com>
2020-02-25 22:11:47 +0000

    enable introspection

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ApolloServer } from 'apollo-server'
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
 
 server.listen(process.env.PORT).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
