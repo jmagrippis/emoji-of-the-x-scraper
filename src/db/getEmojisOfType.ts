d461f31580738aabd24b2dc459cdd74cfa8d0787
Johnny Magrippis <j@magrippis.com>
2020-02-27 10:11:56 +0000

    return correct anchors for every type

--- src/db/getEmojisOfType.ts
+++ src/db/getEmojisOfType.ts
@@ -2,11 +2,18 @@ import { PoolClient } from 'pg'
 
 import { EmojiType } from '../generated/graphql'
 
+const typeToAnchor = {
+  [EmojiType.Day]: "TO_CHAR(created_at, 'YYYY/MM/DD')",
+  [EmojiType.Week]: "TO_CHAR(created_at, 'YYYY/WW')",
+  [EmojiType.Month]: "TO_CHAR(created_at, 'YYYY/MM')",
+}
+
 export const getEmojisOfType = async (
   { type }: { type: EmojiType },
   client: PoolClient
 ) => {
-  const SQL = `SELECT id, character, name, created_at, TO_CHAR(created_at, 'YYYY/MM/DD') as anchor, $1 as type FROM ${type}_emojis
+  const SQL = `SELECT id, character, name, created_at, ${typeToAnchor[type]} as anchor, $1 as type
+    FROM ${type}_emojis
     ORDER BY created_at DESC`
   const { rows } = await client.query(SQL, [type])
 
