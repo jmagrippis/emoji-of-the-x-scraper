9af2b4c21c2a674cdc594ebb9b9825a19f855c72
Johnny Magrippis <j@magrippis.com>
2020-02-29 22:55:02 +0000

    cache responses in memory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { ApolloServer } from 'apollo-server'
+import responseCachePlugin from 'apollo-server-plugin-response-cache'
 
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [responseCachePlugin()],
+  introspection: true,
+})
 
 server.listen(process.env.PORT || 3400).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
