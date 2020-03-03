a09978e47e3e64fc5b7958dad8bb9ec4bb997658
Johnny Magrippis <j@magrippis.com>
2020-03-03 07:39:00 +0000

    that was only filtering one character 🤦🏽‍♂️

--- src/parseEmojis.ts
+++ src/parseEmojis.ts
@@ -6,12 +6,7 @@ type ParsedEmoji = {
 const EMOJI_CAPTURE_REGEX = /<td class=['"]chars['"]>(.+?)<\/td>(.|\n|\r)+?<td class=['"]name['"]>(.+?)<\/td>/gi
 
 export const parseEmojis = (html: string): ParsedEmoji[] =>
-  [...html.matchAll(EMOJI_CAPTURE_REGEX)].reduce<ParsedEmoji[]>(
-    (acc, [, character, , name]) => {
-      if (character !== '🦭') {
-        acc.push({ character, name })
-      }
-      return acc
-    },
-    []
-  )
+  [...html.matchAll(EMOJI_CAPTURE_REGEX)].map(([, character, , name]) => ({
+    character,
+    name,
+  }))
