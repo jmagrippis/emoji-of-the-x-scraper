95f317b8a6e1d460da33ffc155c2ea7cdd77528c
Johnny Magrippis <j@magrippis.com>
2020-03-03 06:50:33 +0000

    we need the name too

--- src/db/getEmojisList.ts
+++ src/db/getEmojisList.ts
@@ -1,7 +1,7 @@
 import { PoolClient } from 'pg'
 
 export const getEmojisList = async (client: PoolClient) => {
-  const SQL = `SELECT id, character FROM emojis_list`
+  const SQL = `SELECT id, character, name FROM emojis_list`
   const { rows } = await client.query(SQL)
 
   return rows
